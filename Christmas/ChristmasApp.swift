54d9081dacd57f281b8951bcce466c316aed11b3
Mariam Joglidze <mariamjoglidze1@gmail.com>
2023-12-23 17:10:07 +0400

    Initial Commit

---
+++ Christmas/ChristmasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChristmasApp.swift
+//  Christmas
+//
+//  Created by Mariam Joglidze on 23.12.23.
+//
+
+import SwiftUI
+
+@main
+struct ChristmasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
